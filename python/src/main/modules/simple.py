38cd2a5f2d6e8a7c6d20681e7c5c169369bec013
Ivo Correia <idvcorreia@gmail.com>
2016-06-14 09:40:04 -0400

    Testing code with scikit-learn

--- python/src/main/modules/simple.py
+++ python/src/main/modules/simple.py
@@ -1,2 +1,6 @@
+from sklearn import svm
+
 def method(value):
+    print 'Using scikit-learn'
+    clf = svm.SVC(gamma=0.001, C=100.)
     return '\nHello world with post with value ' + value + '!\n'
